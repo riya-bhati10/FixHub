371a27368c17aa50c6a3661ee694b2becb88f98e
Lakshya Yadav <lakshyayadav7672@gmail.com>
2026-01-21 15:06:01 +0530

    feat: add auth utilities and refactor auth controller
    - Created auth.utils.js with reusable authentication functions
    - Fixed MongoDB database name to Fixhub_DB
    - Removed code duplication from auth controller
    - Added hashPassword, comparePassword, generateToken utilities

---
+++ backend/utils/auth.utils.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+exports.hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+
+exports.comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+
+exports.generateToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
