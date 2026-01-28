ac4733273fd6a56ddba8dda48a5513466bc7c598
Rupesh Yadav <rupeshyadav6129@gmail.com>
2026-01-28 11:08:26 +0530

    api axios setup is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,33 +15,35 @@ import SignupPage from "./pages/Landing/SignupPage";
 import HowItWorksPage from "./pages/Landing/HowItWorksPage";
 import AboutPage from "./pages/Landing/AboutPage";
 import { TechnicianDashboard } from "./pages/Technician";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/my-booking" element={<FixHubDashboard />} />
-      <Route path="/book-service" element={<BookService />} />
-      <Route path="/profile" element={<ProfileEdi />} />
-      <Route path="/booking-form" element={<BookServiceForm />} />
-      <Route path="/review" element={<ReviewPage />} />
-      <Route path="/booking-success" element={<BookingSuccess />} />
-      <Route path="/cancel-booking" element={<CancelBooking />} />
+      {/* Public Routes */}
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/how-it-works" element={<HowItWorksPage />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/how-it-works" element={<HowItWorksPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/technician/*" element={<TechnicianDashboard />} />
+
+      {/* Customer Protected Routes */}
+      <Route element={<ProtectedRoute allowedRoles={['customer']} />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/my-booking" element={<FixHubDashboard />} />
+        <Route path="/book-service" element={<BookService />} />
+        <Route path="/profile" element={<ProfileEdi />} />
+        <Route path="/booking-form" element={<BookServiceForm />} />
+        <Route path="/review" element={<ReviewPage />} />
+        <Route path="/booking-success" element={<BookingSuccess />} />
+        <Route path="/cancel-booking" element={<CancelBooking />} />
+      </Route>
+
+      {/* Technician Protected Routes */}
+      <Route element={<ProtectedRoute allowedRoles={['technician']} />}>
+        <Route path="/technician/*" element={<TechnicianDashboard />} />
+      </Route>
     </Routes>
-
-
-
   );
 };
 
