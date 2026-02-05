5bc1d163a284cf8e22e185c6d1ffeb082c915367
Vivek Singh Tomar <tomarvivek669@gmail.com>
2026-02-05 14:36:33 +0530

    `Update CORS configuration and axios instance to allow specific origins and credentials`

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ connectDB();
 
 
 const corsOptions = {
-    origin: true, // Allow all origins temporarily
+    origin: [
+        "https://fix-hub-ten.vercel.app",
+        "https://fix-facehnvuw-riya-bhatis-projects.vercel.app",
+        "http://localhost:5173"
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
