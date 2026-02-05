1b525414f5b3e885889fa1475ef249b4360381cf
Vivek Singh Tomar <tomarvivek669@gmail.com>
2026-02-05 14:28:05 +0530

    `Temporarily allow all origins in CORS configuration`

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 
 
 const corsOptions = {
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: true, // Allow all origins temporarily
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
